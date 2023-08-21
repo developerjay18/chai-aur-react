d81f281f89ab92ca87887d3c00449cef28373e54
developerjay18 <jayrai3003@gmail.com>
2023-08-21 09:46:17 +0530

    creating react app and component with npx

---
+++ basic-react-app/src/App.js
@@ -0,0 +1,12 @@
+import Greet from './Greet';
+
+function App() {
+  return (
+    <>
+      <h1>chai aur react | Jay Rai</h1>
+      <Greet />
+    </>
+  );
+}
+
+export default App;
---
+++ basic-react-app/src/Greet.jsx
@@ -0,0 +1,9 @@
+function Greet() {
+  return (
+    <>
+      <h3>Hello Guys, I am a new component</h3>
+    </>
+  );
+}
+
+export default Greet;

47c1165c560555d0d734c896791b1ac7bdb75165
developerjay18 <jayrai3003@gmail.com>
2023-08-21 09:46:54 +0530

    creating react app and component with vite

---
+++ basic-vite-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Greet from './Greet';
+
+function App() {
+  return (
+    <>
+      hello vite react app | Jay Rai
+      <Greet />
+    </>
+  );
+}
+
+export default App;
---
+++ basic-vite-react-app/src/Greet.jsx
@@ -0,0 +1,9 @@
+function Greet() {
+  return (
+    <>
+      <h3>I am new component from Vite react app</h3>
+    </>
+  );
+}
+
+export default Greet;

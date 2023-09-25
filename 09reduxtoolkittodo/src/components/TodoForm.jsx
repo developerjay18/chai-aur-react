ee2f2cb19f6b626464327a0955861389ddbd15e4
developerjay18 <jayrai3003@gmail.com>
2023-09-25 20:15:24 +0530

    updates in todo list functionlity

--- 09reduxtoolkittodo/src/components/TodoForm.jsx
+++ 09reduxtoolkittodo/src/components/TodoForm.jsx
@@ -48,7 +48,7 @@ function TodoForm() {
     <form onSubmit={handleEvent} className="space-x-3 mt-4 w-full font-mono">
       <input
         type="text"
-        id='todo-input'
+        id="todo-input"
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out w-[50%]"
         placeholder="Enter a Todo..."
         value={input}

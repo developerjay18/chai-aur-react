f3bf4226c8dd3e95a928d75971cf0d4908cc726e
developerjay18 <jayrai3003@gmail.com>
2023-09-28 18:34:28 +0530

    setting up redux-toolkit into project

--- 10megablog/src/App.jsx
+++ 10megablog/src/App.jsx
@@ -1,11 +1,38 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice.js';
+import { useDispatch } from 'react-redux';
+import { Header, Footer } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  return (
-    <>
-      <h1>let's be productive 🔥</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return !loading ? (
+    <div className="main-container bg-black min-h-screen text-white">
+      <Header />
+      <Outlet />
+      <h1>Here is my changing content ::mine outlet is above::</h1>
+      <Footer />
+    </div>
+  ) : null;
 }
 
 export default App;
